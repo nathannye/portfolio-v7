478b0b0c5a527442f5d05738dfb198e57481bbf6
Nathan Nye <nathan@nye.design>
2026-04-02 15:40:28 -0600

    space

---
+++ apps/web/src/components/CounterBoxes.tsx
@@ -0,0 +1,11 @@
+import { For } from 'solid-js'
+
+export default function CounterBoxes(props: { count: number }) {
+	return (
+		<div class="inline-grid max-lg:hidden grid-cols-4 gap-3 mt-53">
+			<For each={Array.from({ length: props.count })}>
+				{() => <div class="bg-inverted size-3" />}
+			</For>
+		</div>
+	)
+}
--- apps/web/src/components/ListSection.tsx
+++ apps/web/src/components/ListSection.tsx
@@ -1,5 +1,6 @@
 import type { JSX } from 'solid-js'
 import BracketedText from './BracketedText'
+import CounterBoxes from './CounterBoxes'
 
 type ListSectionProps = {
 	title: string
@@ -11,11 +12,14 @@ export default function ListSection(props: ListSectionProps) {
 	return (
 		<section class="px-margin-1">
 			<div class="flex items-start pt-2 border-t border-inverted/30 flex-col lg:flex-row gap-y-40">
-				<h2 class="w-full flex max-lg:justify-between lg:w-grid-3-w font-[160] mt-9 eyebrow shrink-0">
-					<span class="inline-block">{props.title}</span>
+				<div class="max-lg:w-full">
+					<h2 class="w-full flex max-lg:justify-between lg:w-grid-3-w font-[160] mt-9 eyebrow shrink-0">
+						<span class="inline-block">{props.title}</span>
 
-					<BracketedText class="ml-15">{props.itemCount}</BracketedText>
-				</h2>
+						<BracketedText class="ml-15">{props.itemCount}</BracketedText>
+					</h2>
+					<CounterBoxes count={props.itemCount} />
+				</div>
 				<div class="w-full">{props.children}</div>
 			</div>
 		</section>
