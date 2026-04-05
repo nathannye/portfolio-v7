6dd0d66776129fde6b4e8665b2b0ab3298d38772
Nathan Nye <nathan@nye.design>
2026-04-05 14:05:55 -0600

    els

--- apps/web/src/components/MarginHero.tsx
+++ apps/web/src/components/MarginHero.tsx
@@ -12,16 +12,16 @@ export default function MarginHero(props: MarginHeroProps) {
 	return (
 		<header use:marginHeroAnimation class="px-margin-1 pt-[30vh] pb-[15vh]">
 			<div class="flex max-lg:flex-col-reverse gap-y-40">
-				<dl class="lg:w-grid-2-w max-lg:justify-between	 max-lg:flex gap-x-20 lg:pt-10 shrink-0">
-					<div
+				<div class="lg:w-grid-2-w max-lg:justify-between	 max-lg:flex gap-x-20 lg:pt-10 shrink-0">
+					<dl
 						class="mb-30 eyebrow max-lg:pl-9 max-lg:flex"
 						aria-label={`Estimated reading time ${props.estimatedReadingTime} minutes`}
 					>
-						<dt class="mb-5 max-lg:mr-15">Est.</dt>
-						<dd class="eyebrow opacity-50">{props.estimatedReadingTime}min</dd>
-					</div>
+						<dt class="mb-5 max-lg:mr-15">Est. time</dt>
+						<dd class="eyebrow opacity-50">{props.estimatedReadingTime || 1} min</dd>
+					</dl>
 
-					<div class="eyebrow max-lg:flex max-lg:pl-gutter-1 max-lg:w-grid-3-w gap-x-20">
+					<dl class="eyebrow max-lg:flex max-lg:pl-gutter-1 max-lg:w-grid-3-w gap-x-20">
 						<dt class="max-lg:w-grid-1-w mb-6">Categories</dt>
 						<dd>
 							<ul>
@@ -30,8 +30,8 @@ export default function MarginHero(props: MarginHeroProps) {
 								</For>
 							</ul>
 						</dd>
-					</div>
-				</dl>
+					</dl>
+				</div>
 				<h1 class="heading-2 lg:pr-grid-3-w">{props.title}</h1>
 			</div>
 		</header>
