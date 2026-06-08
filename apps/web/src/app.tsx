734e776ac3491a2ee4812f466f322d3891a0b03f
Nathan Nye <nathan@nye.design>
2026-06-07 22:24:49 -0600

    speed insights

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,7 +1,8 @@
 import { MetaProvider } from '@solidjs/meta'
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
-import { Suspense } from 'solid-js'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+import { onMount, Suspense } from 'solid-js'
 // import GlobalLayout from './components/GlobalLayout'
 import { useViewport } from './hooks/useViewport'
 import './app.css'
@@ -9,6 +10,13 @@ import GlobalLayout from './components/GlobalLayout'
 
 export default function App() {
 	useViewport()
+
+	onMount(() => {
+		if (process.env.NODE_ENV === 'production') {
+			injectSpeedInsights()
+		}
+	})
+
 	return (
 		<Router
 			root={(props) => (
