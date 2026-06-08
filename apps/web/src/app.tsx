02c9dcc6d8fb4399ea51d7d969dd152db9cefda3
Nathan Nye <nathan@nye.design>
2026-06-07 22:27:39 -0600

    app

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,21 +1,13 @@
 import { MetaProvider } from '@solidjs/meta'
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
-
-import { onMount, Suspense } from 'solid-js'
-// import GlobalLayout from './components/GlobalLayout'
+import { Suspense } from 'solid-js'
 import { useViewport } from './hooks/useViewport'
 import './app.css'
 import GlobalLayout from './components/GlobalLayout'
 
 export default function App() {
 	useViewport()
-
-	onMount(() => {
-		if (process.env.NODE_ENV === 'production') {
-			injectSpeedInsights()
-		}
-	})
 
 	return (
 		<Router
