567a254e051add85f9a4aa6cc04129d74dbba519
Nathan Nye <nathan@nye.design>
2026-06-25 14:29:41 -0600

    columns

--- apps/web/src/components/CounterBoxes.tsx
+++ apps/web/src/components/CounterBoxes.tsx
@@ -1,8 +1,9 @@
 import { For } from 'solid-js'
 
 export default function CounterBoxes(props: { count: number }) {
+	
 	return (
-		<div class="inline-grid max-lg:hidden grid-cols-5 gap-3 mt-53">
+		<div class="inline-grid max-lg:hidden grid-cols-4 gap-3 mt-53">
 			<For each={Array.from({ length: props.count })}>
 				{() => <div data-box class="bg-inverted opacity-0 size-3" />}
 			</For>
